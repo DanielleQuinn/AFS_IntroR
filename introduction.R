695cc2ca23d2f062881b9f353496d5340077fa31
Danielle Quinn <danielle.quinn@acadiau.ca>
2019-09-25 15:34:05 -0230

    updated introduction lesson

--- introduction.R
+++ introduction.R
@@ -2,7 +2,12 @@
 # On windows, Ctrl - Enter to run a line of code
 # On Mac Cmd-Enter
 
-# ---- R is a calculator ----
+# You should see > in the lower left hand corner of the Console window
+# If you see + you have missed a piece of code and R is waiting
+## Click in the Console and hit Escape to reset
+# If you see     R is running a process in the background
+
+# ---- Mathematical Operators ----
 5 + 6
 5 - 9
 6 * 7
@@ -10,26 +15,28 @@
 (4 + 5)/6
 
 # ---- Conditional Operators ----
-# Is 5 equal to 5?
+# Logical statements, return TRUE or FALSE
+
+# 5 is equal to 5
 5 == 5
 
-# Is 5 equal to 6?
+# 5 is equal to 6
 5 == 6
 
-# Is 5 greater than 6?
+# 5 is greater than 6
 5 > 6
 
-# Is 5 greater than or equal to 6?
+# 5 is greater than or equal to 6
 5 >= 6
 
-# Is 5 not equal to 6?
+# 5 is not equal to 6
 !5 == 6
 5 != 6
 
-# Is 5 equal to 5 AND 7 greater than 6?
+# 5 is equal to 5 AND 7 is greater than 6
 5 == 5 & 7 > 6
 
-# Is 5 equal to 5 OR 7 greater than 10?
+# 5 is equal to 5 OR 7 is greater than 10
 5 == 5 | 7 > 10
 
 ## Complete each conditional statement so that it returns TRUE
@@ -41,23 +48,30 @@
 # Functions "do things"
 # Recognizable by the function name followed by (
 # Every ( needs a )
+# The information inside the () is called the argument
+
+# FunctionName(argument)
 
 # Goal: Find the square root of 81
 # Function name: sqrt
+# Argument: 81
 sqrt(81)
 
 # Goal: Find the absolute value of 6 - 14
 # Function name: abs
+# Argument: 6 - 14
 abs(6 - 14)
 
-# Nesting functions
 # Goal: Find the square root of the absolute value of 4 - 20
 # Option 1: Evaluate in two steps
-abs(4 - 20)
+abs(4 - 20) # returns 16
 sqrt(16)
+
 # Option 2: Nested functions
+# The output of abs(4 - 20) becomes the argument for sqrt()
 sqrt(abs(4 - 20))
-# Option 3: Pipes - covered later
+
+# Option 3: Pipes - covered later!
 
 # Functions require specific types of arguments
 sqrt("a")
@@ -70,16 +84,24 @@ exp(10) # computes the exponential function
 
 # Arguments are named, but it's not always neccessary to
 # call them by name
+# sqrt() requires an argument named 'x', which we specifiy to be 10
 sqrt(x = 10)
 
 # Some functions use more than one argument
 # It's not always neccessary to provide these
+# There are required arguments and optional arguments
+# Optional arguments have default values
 ?rnorm
 
 # Goal: Return one value from a normal distribution
 # that has a mean of 0 and a standard deviation of 1
+rnorm(n = 1, mean = 0, sd = 1)
 rnorm(n = 1)
-# By default, rnorm uses mean = 0 and sd = 1
+# By default, rnorm uses mean = 0 and sd = 1 so you don't
+# need to specify those values
+
+rnorm() # produces an error
+## What does this error message tell you?
 
 # Goal: Return one value from a normal distribution
 # that has a mean of 10 and a standard deviation of 6
@@ -91,12 +113,12 @@ rnorm(n = 1, sd = 5) # Is this okay?
 
 # ---- Packages ----
 # Packages are collections of functions built by other users
-
+#
 # Step One: Download & install the package to your local library
 install.packages("ggplot2") # one time use
 
 # Step Two: Access the package's functions from the local library
-library(ggplot2) # every time you start RStudio
+library(ggplot2) # every time you start RStudio and want to use these functions
 
 # ---- Finding Help ----
 # Option 1: Google
@@ -106,11 +128,12 @@ library(ggplot2) # every time you start RStudio
 ??average # searching for a keyword in your downloaded packages
 
 # ---- Objects ----
-# Objects ("containers" store information in the form of
-# values and various attributes depending on their structure
+# Objects ("containers") store information
 
-# Goal: Find and use the value of pi
+# Goal: Find the value of the object called pi
 pi
+
+# Goal: Use pi to accomplish a task
 pi * 10
 
 # Goal: Make an object called x that contains the value 15
@@ -120,7 +143,7 @@ x
 
 15 -> x # Can you think of why this might be a bad idea?
 
-# Goal: Make an object called y that contains the value of x plus 3
+## Goal: Make an object called y that contains the value of x plus 3
 y <- x + 3
 y
 
@@ -138,6 +161,11 @@ A <- 100
 c <- a + b
 b <- 50
 c
+
+# (a) 115
+# (b) 25
+# (c) 150
+# (d) 60
 
 # ---- Data Classes ----
 # Numeric
@@ -167,7 +195,8 @@ myvector
 
 # Goal: How many elements are in the vector called myvector?
 length(myvector)
-str(myvector)
+
+# Goal: What class of data is found in myvector?
 class(myvector)
 
 # Goal: Apply a function to all elements in a vector
@@ -180,26 +209,19 @@ myvector > 5
 # Is 11 found in this vector?
 11 %in% myvector
 
-# Goal: Create a vector called myseq that contains all 
-# odd numbers between 1 and 300.
-# Hint: ?seq
-?seq
-myseq <- seq(from = 1, to = 300, by = 2)
-myseq
-
-# How many elements are in myseq?
-length(myseq)
-
-# Is 25 found in myseq?
-25 %in% myseq
-
-# What is the average value of myseq?
-mean(myseq)
-
-# Goal: Create a vector called numbers that contains
+# Goal: Create a vector called mynumbers that contains
 # the numbers 1, 6, 3, 4, and 10
-numbers <- c(1, 6, 3, 4, 10) # "c" means "combine" into a vector
-numbers
+mynumbers <- c(1, 6, 3, 4, 10) # "c" means "combine" into a vector
+mynumbers
+
+## How many elements are in mynumbers?
+length(mynumbers)
+
+## Is 25 found in myseq?
+25 %in% mynumbers
+
+## What is the average value of mynumbers?
+mean(mynumbers)
 
 # Goal: Create a vector called fruit that contains the words apple, orange, and kiwi
 fruit <- c("apple", "orange", "kiwi")
@@ -258,16 +280,25 @@ mean(neweggs, na.rm = TRUE)
 min(neweggs, na.rm = TRUE)
 max(neweggs, na.rm = TRUE)
 
+# Goal: Asses which values are NAs
+# A new function that acts like a conditional operator: is.na()
+
+# Logical statement: each value neweggs is equal to NA
+neweggs == NA # Doesn't work
+is.na(neweggs) # Works
+
+# Logical statement: each value of neweggs is not equal to NA
+!is.na(neweggs)
+
 # ---- Data Structures: Data Frames ----
 # How does all of this relate to the type of data
 # you'll likely have in a wildlife / fisheries context?
-
 
 # Spreadsheets ("flat files") with columns and rows
 # Tidy Data: Each row is an observation, 
 #            Each column is a measurement (variable).
 
-# IMPORTANT: Each column is a vector (atomic) #
+## IMPORTANT: Each column is a vector (atomic) ##
 
 # Goal: Create a data frame with a column called "one" containing the values 1 to 5
 # and a column called "two" containing the letters a to e. Call this data frame mydata
@@ -279,23 +310,34 @@ mydata
 # data frame as a vector using $
 mydata$one # "Show me the contents of the component called one in the object called mydata"
 mydata$two
+mydata$three # Does not exist
 
 # Then you can treat it like a regular vector
 # Goal: What is the average value in the column called one?
 mean(mydata$one)
 
-# ---- Importing Data ----
-# Goal: Import a .txt file
+# ---- Import Data ----
+# Goal: Import a .csv file
 
-## Step One: Tell R where to look
-setwd("C:/Users/Danielle/Desktop/AFS_IntroR")
+# 1. Check your working directory
+getwd()
 
-## Step Two: Load the data
-species <- read.delim("species.txt")
-species
+# 2. List files in your working directory
+list.files()
 
-# What class is the object called species?
-class(species)
+# 3. Import the data
+data <- read.csv("KingsCreekFishSurveys.csv")
 
-# View species in a new tab
-View(species)
+# ---- Exploring Data Frames ----
+View(data) # View data in a new tab
+
+dim(data) # Number of rows and columns
+nrow(data) # Number of rows
+ncol(data) # Number of columns
+
+head(data) # Display the first six rows
+tail(data) # Display the last six rows
+names(data) # Display the names of each column
+
+str(data) # Display the structure of the object
+summary(data) # Summarise each column
